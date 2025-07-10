dcd67461d10433cffb1342d1970428eef5c64f35
YeonwooSung <neos960518@gmail.com>
2025-07-10 22:40:34 +0900

    feat: Use docker for deployment

--- run_db.sh
+++ run_db.sh
@@ -2,4 +2,4 @@
 docker build -t mangasearch-paradedb -f docker/Dockerfile.db .
 
 # 컨테이너 실행
-docker run --name mangasearch-paradedb -e POSTGRES_PASSWORD=password mangasearch-paradedb
+docker run --name mangasearch-paradedb -e POSTGRES_PASSWORD=password -p 5432:5432 -d mangasearch-paradedb
